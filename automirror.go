ea57ed42518f94f79e16868c82dd5f3190c0b261
alexis <fauvel.alexis@gmail.com>
2019-10-27 23:56:24 +0100

    JFrog pusher and Mirror code

--- automirror.go
+++ automirror.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"automirror/configs"
+	"automirror/mirrors"
 	"automirror/pullers"
+	"automirror/pushers"
 )
 
 func main() {
 	configManager := configs.TomlConfigManager{File: "config.toml"}
 	config := configManager.Parse()
 
-	var pullerArray []pullers.Puller
+	var mirrorsArray []mirrors.Mirror
 	for name, mirror := range config.Mirrors {
 		//var puller pullers.Puller
 		//puller = reflect.TypeOf(module.Puller).New()
@@ -18,16 +20,23 @@ func main() {
 		//reflect.ValueOf(&puller).MethodByName(module.Puller).Call([]reflect.Value{})
 		//reflect.TypeOf(&puller).MethodByName(module.Puller)
 		//puller = new(reflect.TypeOf(&puller))
-		pullerArray = append(pullerArray, pullers.Maven{
-			Url:              mirror.Puller.Source,
-			Folder:           mirror.Puller.Destination,
-			MetadataFileName: "maven-metadata.xml",
-			DatabaseFile:     "maven.db",
-			Artifacts:        []pullers.Artifact{{"com.airbnb", "deeplinkdispatch", "4.0.0"}},
+		mirrorsArray = append(mirrorsArray, mirrors.Mirror{
+			Puller: pullers.Maven{
+				Url:              mirror.Puller.Source,
+				Folder:           mirror.Puller.Destination,
+				MetadataFileName: "maven-metadata.xml",
+				DatabaseFile:     "maven.db",
+				Artifacts:        []pullers.Artifact{{"com.airbnb", "deeplinkdispatch", "4.0.0"}},
+			},
+			Pusher: pushers.JFrog{
+				Url:    "http://localhost:8081/artifactory/test",
+				ApiKey: "AKCp5e2qXnFDWrtw7hJHjjWxR6ei5tCQ3HCvdnSYop6Y8w1vK1GQeUEKeFqSePJXmpCHexcac",
+				Source: "tmp/maven",
+			},
 		})
 	}
 
-	for _, puller := range pullerArray {
-		puller.Pull()
+	for _, mirror := range mirrorsArray {
+		mirror.Run()
 	}
 }
--- mirrors/Mirror.go
+++ mirrors/Mirror.go
@@ -1,1 +1,16 @@
 package mirrors
+
+import (
+	"automirror/pullers"
+	"automirror/pushers"
+)
+
+type Mirror struct {
+	Puller pullers.Puller
+	Pusher pushers.Pusher
+}
+
+func (m Mirror) Run() {
+	m.Puller.Pull()
+	m.Pusher.Push()
+}
