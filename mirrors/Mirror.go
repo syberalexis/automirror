a62df53a9fef5d706617a8a39caabe509d6506c3
alexis <fauvel.alexis@gmail.com>
2019-11-10 23:36:53 +0100

    Test Travis

--- mirrors/Mirror.go
+++ mirrors/Mirror.go
@@ -34,13 +34,13 @@ func (m Mirror) Run() {
 		if m.Puller != nil {
 			count, err := m.Puller.Pull()
 			if err != nil {
-				log.Errorf("The %s mirror stop to pull (%s elements). This is due to : %s", m.Name, count, err)
+				log.Errorf("The %s mirror stop to pull (%d elements). This is due to : %s", m.Name, count, err)
 			}
 		}
 		if m.Pusher != nil {
 			err := m.Pusher.Push()
 			if err != nil {
-				log.Errorf("The %s mirror stop to push. This is due to : %s", m.Name, nil)
+				log.Errorf("The %s mirror stop to push. This is due to : %s", m.Name, err)
 			}
 		}
 		m.IsRunning = false
