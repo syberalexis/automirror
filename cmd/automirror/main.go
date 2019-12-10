fb05e3bd9e39a9d9d9399dc76a7ad2a3ab250cdc
alexis <fauvel.alexis@gmail.com>
2019-12-10 01:10:03 +0100

    First commands to manage mirrors

--- cmd/automirror/main.go
+++ cmd/automirror/main.go
@@ -3,26 +3,55 @@ package main
 import (
 	"fmt"
 	"github.com/pkg/errors"
+	"github.com/syberalexis/automirror/pkg/core"
 	"gopkg.in/alecthomas/kingpin.v2"
 	"os"
 	"path/filepath"
 )
 
-var (
-	app    = kingpin.New(filepath.Base(os.Args[0]), "Automirror is a software to download packages from internet into local.")
-	config = app.Flag("config", "Config file").Short('c').String()
-)
-
 func main() {
+	automirror := &core.Automirror{}
+
+	// Globals
+	app := kingpin.New(filepath.Base(os.Args[0]), "Automirror is a software to download packages from internet into local.")
+	app.Flag("config", "Config file").Default("/etc/automirror/config.toml").Short('c').StringVar(&automirror.Config)
+
+	// Mirror commands
+	mirrorCommand := app.Command("mirror", "Commands to manage a mirror")
 	app.HelpFlag.Short('h')
 	app.Version("0.0.1")
 
-	_, err := app.Parse(os.Args[1:])
+	// Mirrors list
+	mirrorCommand.Command("list", "List mirrors process").
+		Action(func(c *kingpin.ParseContext) error { return automirror.GetMirrors() })
+
+	// Mirror start command
+	mirrorStartCommand := mirrorCommand.Command("start", "Start a mirror process").
+		Action(func(c *kingpin.ParseContext) error { return automirror.Start(*c.Elements[2].Value) })
+	mirrorStartCommand.Arg("name", "mirror name").Required().String()
+
+	// Mirror status command
+	mirrorStatusCommand := mirrorCommand.Command("status", "Status of a mirror process").
+		Action(func(c *kingpin.ParseContext) error { return automirror.Status(*c.Elements[2].Value) })
+	mirrorStatusCommand.Arg("name", "mirror name").Required().String()
+
+	// Mirror stop command
+	mirrorStopCommand := mirrorCommand.Command("stop", "Stop a mirror process").
+		Action(func(c *kingpin.ParseContext) error { return automirror.Stop(*c.Elements[2].Value) })
+	mirrorStopCommand.Arg("name", "mirror name").Required().String()
+
+	// Mirror restart command
+	mirrorRestartCommand := mirrorCommand.Command("restart", "Restart a mirror process").
+		Action(func(c *kingpin.ParseContext) error { return automirror.Restart(*c.Elements[2].Value) })
+	mirrorRestartCommand.Arg("name", "mirror name").Required().String()
+
+	// Parsing
+	args, err := app.Parse(os.Args[1:])
 	if err != nil {
-		fmt.Fprintln(os.Stderr, errors.Wrapf(err, "Error parsing commandline arguments"))
+		_, err = fmt.Fprintln(os.Stderr, errors.Wrapf(err, "Error parsing commandline arguments"))
 		app.Usage(os.Args[1:])
 		os.Exit(2)
 	} else {
-		fmt.Fprintln(os.Stdout, *config)
+		kingpin.MustParse(args, err)
 	}
 }
--- pkg/core/automirror.go
+++ pkg/core/automirror.go
@@ -1,148 +1,172 @@
 package core
 
 import (
-	log "github.com/sirupsen/logrus"
-	"github.com/syberalexis/automirror/pkg/both"
-	"github.com/syberalexis/automirror/pkg/configs"
+	"fmt"
 	"github.com/syberalexis/automirror/pkg/mirrors"
-	"github.com/syberalexis/automirror/pkg/pullers"
-	"github.com/syberalexis/automirror/pkg/pushers"
-	"github.com/syberalexis/automirror/utils/filesystem"
-	"github.com/syberalexis/automirror/utils/logger"
-	"os"
-	"sync"
+	"strings"
 )
 
-type automirror struct {
-	config  configs.TomlConfig
+type Automirror struct {
+	Config  string
 	mirrors []mirrors.Mirror
 }
 
-func (a *automirror) buildMirrors() {
-	var mirrorsArray []mirrors.Mirror
-	for name, mirror := range a.config.Mirrors {
-		var puller pullers.Puller
-		var pusher pushers.Pusher
-
-		if engine := buildEngine(mirror.Puller); engine != nil {
-			puller = engine.(pullers.Puller)
-		}
-		if engine := buildEngine(mirror.Pusher); engine != nil {
-			pusher = engine.(pushers.Pusher)
-		}
-
-		mirrorsArray = append(
-			mirrorsArray,
-			mirrors.New(
-				name,
-				puller,
-				pusher,
-				mirror.Timer,
-				logger.LoggerInfo{
-					Directory: a.config.LogDir,
-					Filename:  name,
-					Format:    a.config.LogFormat,
-					Level:     a.config.LogLevel,
-				},
-			),
-		)
+func (a *Automirror) GetMirrors() error {
+	var mirrorList []string
+	for _, mirror := range a.mirrors {
+		mirrorList = append(mirrorList, mirror.Name)
 	}
-	a.mirrors = mirrorsArray
+	_, err := fmt.Println("[", strings.Join(mirrorList, ","), "]")
+	return err
 }
 
-func initializeLogger(config configs.TomlConfig) *os.File {
-	var file *os.File
-	if config.LogDir != "" {
-		file, err := os.OpenFile(filesystem.Combine(config.LogDir, "automirror.log"), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.SetOutput(file)
-	}
-	if config.LogFormat == "json" {
-		log.SetFormatter(&log.JSONFormatter{})
-	}
-	if config.LogLevel != "" {
-		var level log.Level
-		ptr := &level
-		err := ptr.UnmarshalText([]byte(config.LogLevel))
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.SetLevel(level)
-	}
-
-	return file
+func (a *Automirror) Start(mirror string) error {
+	return nil
 }
 
-func readConfiguration(configFile string) configs.TomlConfig {
-	var config configs.TomlConfig
-	err := configs.Parse(&config, configFile)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return config
+func (a *Automirror) Status(mirror string) error {
+	return nil
 }
 
-func buildEngine(config configs.EngineConfig) interface{} {
-	var engine interface{}
-	var err error
-
-	switch config.Name {
-	case "deb":
-		engine, err = pullers.NewDeb(config)
-	case "docker":
-		engine, err = both.NewDocker(config)
-	case "git":
-		engine, err = both.NewGit(config)
-	case "mvn":
-		engine, err = pullers.NewMaven(config)
-	case "pip":
-		engine, err = pullers.NewPython(config)
-	case "rsync":
-		engine, err = both.NewRsync(config)
-	case "wget":
-		engine, err = pullers.NewWget(config)
-	case "jfrog":
-		engine, err = pushers.NewJFrog(config)
-	default:
-		engine = nil
-	}
-
-	if err != nil {
-		log.Error(err)
-	}
-
-	return engine
+func (a *Automirror) Stop(mirror string) error {
+	return nil
 }
 
-func main() {
-	var configFile = "/etc/automirror/config.toml"
-
-	args := os.Args[1:]
-	if len(args) > 0 {
-		configFile = args[0]
-	}
-
-	// Read configuration
-	automirror := automirror{config: readConfiguration(configFile)}
-
-	// Logging
-	file := initializeLogger(automirror.config)
-	defer file.Close()
-	defer logger.CloseLoggers()
-
-	// Build mirrors
-	automirror.buildMirrors()
-
-	// Run mirrors
-	var wg sync.WaitGroup
-	for _, mirror := range automirror.mirrors {
-		wg.Add(1)
-		go func(mirror mirrors.Mirror) {
-			defer wg.Done()
-			mirror.Start()
-		}(mirror)
-	}
-	wg.Wait()
+func (a *Automirror) Restart(mirror string) error {
+	return nil
 }
+
+//type Automirror struct {
+//	config  configs.TomlConfig
+//	mirrors []mirrors.Mirror
+//}
+//
+//
+//func (a *Automirror) buildMirrors() {
+//	var mirrorsArray []mirrors.Mirror
+//	for name, mirror := range a.config.Mirrors {
+//		var puller pullers.Puller
+//		var pusher pushers.Pusher
+//
+//		if engine := buildEngine(mirror.Puller); engine != nil {
+//			puller = engine.(pullers.Puller)
+//		}
+//		if engine := buildEngine(mirror.Pusher); engine != nil {
+//			pusher = engine.(pushers.Pusher)
+//		}
+//
+//		mirrorsArray = append(
+//			mirrorsArray,
+//			mirrors.New(
+//				name,
+//				puller,
+//				pusher,
+//				mirror.Timer,
+//				logger.LoggerInfo{
+//					Directory: a.config.LogDir,
+//					Filename:  name,
+//					Format:    a.config.LogFormat,
+//					Level:     a.config.LogLevel,
+//				},
+//			),
+//		)
+//	}
+//	a.mirrors = mirrorsArray
+//}
+//
+//func initializeLogger(config configs.TomlConfig) *os.File {
+//	var file *os.File
+//	if config.LogDir != "" {
+//		file, err := os.OpenFile(filesystem.Combine(config.LogDir, "automirror.log"), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+//		if err != nil {
+//			log.Fatal(err)
+//		}
+//		log.SetOutput(file)
+//	}
+//	if config.LogFormat == "json" {
+//		log.SetFormatter(&log.JSONFormatter{})
+//	}
+//	if config.LogLevel != "" {
+//		var level log.Level
+//		ptr := &level
+//		err := ptr.UnmarshalText([]byte(config.LogLevel))
+//		if err != nil {
+//			log.Fatal(err)
+//		}
+//		log.SetLevel(level)
+//	}
+//
+//	return file
+//}
+//
+//func readConfiguration(configFile string) configs.TomlConfig {
+//	var config configs.TomlConfig
+//	err := configs.Parse(&config, configFile)
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	return config
+//}
+//
+//func buildEngine(config configs.EngineConfig) interface{} {
+//	var engine interface{}
+//	var err error
+//
+//	switch config.Name {
+//	case "deb":
+//		engine, err = pullers.NewDeb(config)
+//	case "docker":
+//		engine, err = both.NewDocker(config)
+//	case "git":
+//		engine, err = both.NewGit(config)
+//	case "mvn":
+//		engine, err = pullers.NewMaven(config)
+//	case "pip":
+//		engine, err = pullers.NewPython(config)
+//	case "rsync":
+//		engine, err = both.NewRsync(config)
+//	case "wget":
+//		engine, err = pullers.NewWget(config)
+//	case "jfrog":
+//		engine, err = pushers.NewJFrog(config)
+//	default:
+//		engine = nil
+//	}
+//
+//	if err != nil {
+//		log.Error(err)
+//	}
+//
+//	return engine
+//}
+//
+//func main() {
+//	var configFile = "/etc/automirror/config.toml"
+//
+//	args := os.Args[1:]
+//	if len(args) > 0 {
+//		configFile = args[0]
+//	}
+//
+//	// Read configuration
+//	automirror := Automirror{config: readConfiguration(configFile)}
+//
+//	// Logging
+//	file := initializeLogger(automirror.config)
+//	defer file.Close()
+//	defer logger.CloseLoggers()
+//
+//	// Build mirrors
+//	automirror.buildMirrors()
+//
+//	// Run mirrors
+//	var wg sync.WaitGroup
+//	for _, mirror := range automirror.mirrors {
+//		wg.Add(1)
+//		go func(mirror mirrors.Mirror) {
+//			defer wg.Done()
+//			mirror.Start()
+//		}(mirror)
+//	}
+//	wg.Wait()
+//}
